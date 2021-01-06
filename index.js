19b4f3d497d091b6fa641a5ccfef3e87b1b8c6f9
Usama Irfan <usama.irfan9797@gmail.com>
2021-01-06 14:20:19 +0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var qrcode = new QRCode("qrcode", {
+   colorDark: "#000",
+   colorLight: "#fff",
+ });
+
+const makeCode = () => {
+var Text = document.getElementById("text");
+ 
+   if (!Text.value) {
+      alert("Input a text");
+      Text.focus();
+      return;
+   }
+ 
+   qrcode.makeCode(Text.value);
+};
+ 
+$("#text")
+  .on("blur", function () {
+      makeCode();
+  })
+  .on("keydown", function (e) {
+      if (e.keyCode == 13) {
+         makeCode();
+      }
+});
+
+ 
